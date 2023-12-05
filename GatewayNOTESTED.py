6fd2cfd6e790f376aa7c33a199c85b314317a033
luquech <79807018+luquech@users.noreply.github.com>
2023-12-05 17:42:53 -0300

    Update GatewayNOTESTED.py

--- GatewayNOTESTED.py
+++ GatewayNOTESTED.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, db
 import time
 import bluetooth
 import datetime
+import threading
 
 # Configuração do Firebase
 cred = credentials.Certificate("/home/lucas/Downloads/projetoiot-6f20f-firebase-adminsdk-zwnv8-d892e9e87c.json")
@@ -32,7 +33,7 @@ gateway_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 gateway_sock.connect((gateway_address, gateway_channel))
 
 # Configuração do dispositivo Bluetooth (ESPLDR)
-ldr_device_name = "ESPLDR"  # Substitua pelo nome do seu dispositivo Bluetooth
+ldr_device_name = "ESP32_LDR"  # Substitua pelo nome do seu dispositivo Bluetooth
 
 # Descubra o endereço Bluetooth do dispositivo ESPLDR
 ldr_address = discover_device_address(ldr_device_name)
@@ -49,57 +50,77 @@ def control_lights(value):
 
 def read_ldr_data():
     ldr_data = ldr_sock.recv(1024).decode()
-    return ldr_data
+
+    # Extrai o valor numérico da string
+    numeric_value_str = ldr_data.strip()
+
+    return numeric_value_str
+def read_and_send():
+    while True:
+        luz_state = db.reference("luz").get()
+        print("Luz State:", luz_state)
+
+        # Enviar o estado da luz para o segundo ESP32
+        control_lights("LOW" if luz_state else "HIGH")
 
 def upload_ldr_data():
     current_ldr_data_str = read_ldr_data()
-    
+
     try:
-        current_ldr_data_float = float(current_ldr_data_str)
-        print("LDR Data (float):", current_ldr_data_float)
-        
+        # Remove o prefixo "LDR Value:" e então converte para float
+        current_ldr_data_float = float(current_ldr_data_str.replace("LDR Value:", "").strip())
+        print("Dados LDR (float):", current_ldr_data_float)
+
         # Enviar os dados LDR como float para o caminho "LDRATUAL" do Firebase
         db.reference("LDRATUAL").set(current_ldr_data_float)
-        
+
     except ValueError:
         print(f"Erro ao converter os dados LDR: {current_ldr_data_str} não é um valor válido em ponto flutuante.")
 
-# Restante do código...
 
 def log_ldr_data():
     while True:
         # Aguardar 30 minutos
-        time.sleep(1800)
+        time.sleep(15)
 
         # Obter dados LDR
         current_ldr_data_str = read_ldr_data()
 
         try:
-            # Tentar converter os dados LDR para float
-            current_ldr_data_float = float(current_ldr_data_str)
-            
             # Obter data e hora atual no formato desejado
             current_datetime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
-            # Criar registro com apenas o valor do LDR
-            log_entry = {"value": current_ldr_data_float}
+            # Converter o valor LDR para float
+            current_ldr_data_float = float(current_ldr_data_str)
 
-            # Adicionar registro ao caminho "LDR" no Firebase com a chave única como data e hora
-            db.reference(f"LDR/{current_datetime}").set(log_entry)
+            # Referência ao caminho "LDR" no Firebase
+            ldr_ref = db.reference("LDR")
+
+            # Adicionar registro ao caminho "LDR" no Firebase
+            ldr_ref.child(current_datetime).set(current_ldr_data_float)
+
+            # Adicione um print para exibir a informação que será armazenada em db.reference
+            print(f"Valor armazenado em db.reference: {current_datetime} - {current_ldr_data_float}")
 
         except ValueError:
-            print(f"Erro ao converter os dados LDR para float: {current_ldr_data_str}")
+            print(f"Erro ao processar os dados LDR: {current_ldr_data_str}")
 
+
+# Iniciar a função de log em uma thread separada
+log_thread = threading.Thread(target=log_ldr_data)
+log_thread.start()
 try:
-    upload_ldr_data()
-
+    while True:
+        upload_ldr_data()
+        read_and_send()
+        time.sleep(5)  # Aguarda 10 segundos entre cada iteração
 except KeyboardInterrupt:
     print("Programa encerrado.")
 finally:
     gateway_sock.close()
     ldr_sock.close()
 
-# Iniciar a função de log em uma thread separada
-import threading
-log_thread = threading.Thread(target=log_ldr_data)
-log_thread.start()
+
+
+
+
